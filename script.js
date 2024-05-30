42d00b1c5b4cbaf9b8631dced111176325dcbf68
Spencer Johnson <123844048+SpencerJCode@users.noreply.github.com>
2024-05-30 13:32:41 -0400

    Slides of pictures can now be advanced by clicking.

--- script.js
+++ script.js
@@ -27,56 +27,116 @@ function showSonaSlides() {
   setTimeout(showSonaSlides, timeout);
 }
 
-function showKoeySlides() {
-    let i;
-    let slides = document.getElementsByClassName("koeySlides");
-    let dots = document.getElementsByClassName("dotKoey");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-    }
-    slideIndexKoey++;
-    if (slideIndexKoey > slides.length) {slideIndexKoey = 1}    
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndexKoey-1].style.display = "block";  
-    dots[slideIndexKoey-1].className += " active";
-    setTimeout(showKoeySlides, timeout); 
+function advanceSonaSlides() {
+  let slides = document.getElementsByClassName("sonaSlides");
+  let dots = document.getElementsByClassName("dotSona");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
   }
-  
-  function showHavenSlides() {
-    let i;
-    let slides = document.getElementsByClassName("havenSlides");
-    let dots = document.getElementsByClassName("dotHaven");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-    }
-    slideIndexHaven++;
-    if (slideIndexHaven > slides.length) {slideIndexHaven = 1}    
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndexHaven-1].style.display = "block";  
-    dots[slideIndexHaven-1].className += " active";
-    setTimeout(showHavenSlides, timeout); 
+  slideIndexSona++;
+  if (slideIndexSona > slides.length) {slideIndexSona = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
   }
+  slides[slideIndexSona-1].style.display = "block";  
+  dots[slideIndexSona-1].className += " active";
+}
 
-  function showCalgarySlides() {
-    let i;
-    let slides = document.getElementsByClassName("calgarySlides");
-    let dots = document.getElementsByClassName("dotCalgary");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-    }
-    slideIndexCalgary++;
-    if (slideIndexCalgary > slides.length) {slideIndexCalgary = 1}    
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndexCalgary-1].style.display = "block";  
-    dots[slideIndexCalgary-1].className += " active";
-    setTimeout(showCalgarySlides, timeout); 
+function showKoeySlides() {
+  let i;
+  let slides = document.getElementsByClassName("koeySlides");
+  let dots = document.getElementsByClassName("dotKoey");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
   }
+  slideIndexKoey++;
+  if (slideIndexKoey > slides.length) {slideIndexKoey = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndexKoey-1].style.display = "block";  
+  dots[slideIndexKoey-1].className += " active";
+  setTimeout(showKoeySlides, timeout); 
+}
+
+function advanceKoeySlides() {
+  let slides = document.getElementsByClassName("koeySlides");
+  let dots = document.getElementsByClassName("dotKoey");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndexKoey++;
+  if (slideIndexKoey > slides.length) {slideIndexKoey = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndexKoey-1].style.display = "block";  
+  dots[slideIndexKoey-1].className += " active";
+}
+  
+function showHavenSlides() {
+  let i;
+  let slides = document.getElementsByClassName("havenSlides");
+  let dots = document.getElementsByClassName("dotHaven");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndexHaven++;
+  if (slideIndexHaven > slides.length) {slideIndexHaven = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndexHaven-1].style.display = "block";  
+  dots[slideIndexHaven-1].className += " active";
+  setTimeout(showHavenSlides, timeout); 
+}
+
+function advanceHavenSlides() {
+  let slides = document.getElementsByClassName("havenSlides");
+  let dots = document.getElementsByClassName("dotHaven");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndexHaven++;
+  if (slideIndexHaven > slides.length) {slideIndexHaven = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndexHaven-1].style.display = "block";  
+  dots[slideIndexHaven-1].className += " active";
+}
+
+function showCalgarySlides() {
+  let i;
+  let slides = document.getElementsByClassName("calgarySlides");
+  let dots = document.getElementsByClassName("dotCalgary");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndexCalgary++;
+  if (slideIndexCalgary > slides.length) {slideIndexCalgary = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndexCalgary-1].style.display = "block";  
+  dots[slideIndexCalgary-1].className += " active";
+  setTimeout(showCalgarySlides, timeout); 
+}
+
+function advanceCalgarySlides() {
+  let slides = document.getElementsByClassName("calgarySlides");
+  let dots = document.getElementsByClassName("dotCalgary");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndexCalgary++;
+  if (slideIndexCalgary > slides.length) {slideIndexCalgary = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndexCalgary-1].style.display = "block";  
+  dots[slideIndexCalgary-1].className += " active";
+}
   
 function showPastLitterSlides() {
     let i;
@@ -88,4 +148,14 @@ function showPastLitterSlides() {
     if (slideIndexPastLitter > slides.length) {slideIndexPastLitter = 1}    
     slides[slideIndexPastLitter-1].style.display = "block";  
     setTimeout(showPastLitterSlides, timeout-2000);
+}
+
+function advancePastLitterSlides() {
+  let slides = document.getElementsByClassName("pastLitterSlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndexPastLitter++;
+  if (slideIndexPastLitter > slides.length) {slideIndexPastLitter = 1}    
+  slides[slideIndexPastLitter-1].style.display = "block";  
 }
